6591738b12d8d037314f62bd643a3619b0dc686c
DentistOmarAshraf <omar_ashraf@live.com>
2024-09-02 19:53:21 +0300

    try

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -27,6 +27,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get Page dataset
+        """
         assert isinstance(page, int)
         assert page > 0
         assert isinstance(page_size, int)
