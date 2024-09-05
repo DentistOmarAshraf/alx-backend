f1597708dd7c358b6ab73239901c02c570f5b9d3
DentistOmarAshraf <omar_ashraf@live.com>
2024-09-05 07:04:03 +0300

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -4,6 +4,7 @@ FIFOCache - class of cache based on FIFO algorithm
 to discard items
 """
 from base_caching import BaseCaching
+from collections import deque
 
 
 class FIFOCache(BaseCaching):
@@ -13,6 +14,7 @@ class FIFOCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
+        self.key_list = deque([])
 
     def put(self, key, item):
         """add item to parent class dict
@@ -21,8 +23,14 @@ class FIFOCache(BaseCaching):
             return None
         self.cache_data[key] = item
 
+        if key not in self.key_list:
+            self.key_list.append(key)
+        else:
+            self.key_list.remove(key)
+            self.key_list.append(key)
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_element_key = next(iter(self.cache_data))
+            first_element_key = self.key_list.popleft()
             del (self.cache_data[first_element_key])
             print('DISCARD: {}'.format(first_element_key))
 
