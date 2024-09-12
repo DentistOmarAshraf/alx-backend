dd74ec74392e792db8042ac4ae1230874df3514f
DentistOmarAshraf <omar_ashraf@live.com>
2024-09-12 10:46:45 +0300

    4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Flask app - Using Babel
+"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+from typing import Any
+
+
+class Config:
+    """
+    App Config
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> Any:
+    """preferd language from request header (Accept Language)
+    """
+    prefered_lang = request.args.get('locale', None)
+    if prefered_lang and prefered_lang in Config.LANGUAGES:
+        return prefered_lang
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/", strict_slashes=False, methods=['GET'])
+def home() -> Any:
+    """index page
+    """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    """Main"""
+    app.run(host='0.0.0.0', port=5000)
