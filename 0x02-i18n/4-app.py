c5a8dec768bb174cb8277a8f57f7995da89482ae
DentistOmarAshraf <omar_ashraf@live.com>
2024-09-12 11:01:23 +0300

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -35,7 +35,7 @@ def get_locale() -> Any:
 def home() -> Any:
     """index page
     """
-    return render_template("3-index.html")
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
